3a0facc8ca1e6cc89301cb72d2142cdb5a1164b7
Josephchinedu <47852925+Josephchinedu@users.noreply.github.com>
2020-08-21 10:43:27 +0100

    Updates

---
+++ precache-manifest.2f29f41ee370901b34ab20b030379c1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900bfa32b52db161a2c57c2da6150171",
+    "url": "/react-bootstrap/index.html"
+  },
+  {
+    "revision": "6b7762cee7fe10bb0c75",
+    "url": "/react-bootstrap/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85aa8e0c3f31c01bbdf1",
+    "url": "/react-bootstrap/static/js/2.1f48b507.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-bootstrap/static/js/2.1f48b507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b7762cee7fe10bb0c75",
+    "url": "/react-bootstrap/static/js/main.8e53189c.chunk.js"
+  },
+  {
+    "revision": "d6713907c2dcbb200d30",
+    "url": "/react-bootstrap/static/js/runtime-main.f786c2f2.js"
+  },
+  {
+    "revision": "24eed4ffae8232784c10d780d2405215",
+    "url": "/react-bootstrap/static/media/bo.24eed4ff.jpeg"
+  }
+]);
